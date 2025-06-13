94f668fa948644c2be77fe8e1d5d5189fcfbe62d
Rafiullah khan <rafiullah.khan@contour-software.com>
2025-06-13 12:11:01 +0500

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "aci-rg"
+  location = "East US"
+}
+
+resource "azurerm_container_group" "aci" {
+  name                = "my-container"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+
+  container {
+    name   = "mywebapp"
+    image  = "rafikhan110/my-web-app:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  ip_address_type = "Public"
+  dns_name_label  = "aci-${random_id.suffix.hex}"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
